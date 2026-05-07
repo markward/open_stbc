686198415873c16fd55efa193ff8ba3b07f5c599
Mark Ward <mark.ward@gmail.com>
2026-05-07 12:11:14 +0100

    feat: implement Game/Episode/Mission hierarchy

---
+++ engine/core/game.py
@@ -0,0 +1,41 @@
+from engine.core.ids import TGObject
+from engine.appc.events import TGEventHandlerObject
+
+_current_game: "Game | None" = None
+
+
+def Game_GetCurrentGame() -> "Game | None":
+    return _current_game
+
+
+def _set_current_game(game: "Game | None") -> None:
+    global _current_game
+    _current_game = game
+
+
+class Mission(TGEventHandlerObject):
+    pass
+
+
+class Episode(TGObject):
+    def __init__(self):
+        super().__init__()
+        self._current_mission: Mission | None = None
+
+    def GetCurrentMission(self) -> Mission | None:
+        return self._current_mission
+
+    def SetCurrentMission(self, mission: Mission) -> None:
+        self._current_mission = mission
+
+
+class Game(TGObject):
+    def __init__(self):
+        super().__init__()
+        self._current_episode: Episode | None = None
+
+    def GetCurrentEpisode(self) -> Episode | None:
+        return self._current_episode
+
+    def SetCurrentEpisode(self, episode: Episode) -> None:
+        self._current_episode = episode
---
+++ tests/unit/test_game.py
@@ -0,0 +1,26 @@
+import pytest
+from engine.core.game import Game, Episode, Mission, Game_GetCurrentGame, _set_current_game
+from engine.appc.events import TGEventHandlerObject
+
+
+def test_game_episode_mission_chain():
+    mission = Mission()
+    episode = Episode()
+    episode.SetCurrentMission(mission)
+    game = Game()
+    game.SetCurrentEpisode(episode)
+    _set_current_game(game)
+
+    assert Game_GetCurrentGame() is game
+    assert Game_GetCurrentGame().GetCurrentEpisode() is episode
+    assert Game_GetCurrentGame().GetCurrentEpisode().GetCurrentMission() is mission
+
+
+def test_no_game_returns_none():
+    _set_current_game(None)
+    assert Game_GetCurrentGame() is None
+
+
+def test_mission_is_event_handler():
+    mission = Mission()
+    assert isinstance(mission, TGEventHandlerObject)
